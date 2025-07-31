d7e758ac95b3bc688a3687e12afc613aacd33f88
Kaleb Elwert <belak@coded.io>
2025-07-31 15:29:59 -0700

    Override deploy-rs from nixpkgs so it can use the binary cache

--- overlays.nix
+++ overlays.nix
@@ -1,4 +1,5 @@
 {
+  nixpkgs-unstable,
   agenix,
   deploy-rs,
   ...
@@ -14,4 +15,21 @@
   agenix = agenix.overlays.default;
 
   deploy-rs = deploy-rs.overlays.default;
+
+  unstable = final: _prev: { unstable = import nixpkgs-unstable { inherit (final) config system; }; };
+
+  # This is a modified version of what's in the deploy-rs readme. For some
+  # reason overriding deploy-rs entirely seems to clobber the needed `lib` attr,
+  # and oddly enough the `deploy-rs` binary is accessed through
+  # deploy-rs.reploy-rs, so we need to provide that as well.
+  #
+  # Note that this depends on our "unstable" overlay because it's the easiest
+  # way to make sure we get a package from nixpkgs and not from the deploy-rs
+  # source. Another option would be creating a new nixpkgs instance.
+  deploy-rs-pkg-override = final: prev: {
+    deploy-rs = final.unstable.deploy-rs // {
+      inherit (final.unstable) deploy-rs;
+      inherit (prev.deploy-rs) lib;
+    };
+  };
 }
