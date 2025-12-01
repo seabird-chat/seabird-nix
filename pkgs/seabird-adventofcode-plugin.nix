e2747005fb04e69b55798d1566d30cc9f1284269
Kaleb Elwert <belak@coded.io>
2025-12-01 09:50:54 -0800

    Bump seabird-adventofcode-plugin to v0.2.4

--- pkgs/seabird-adventofcode-plugin.nix
+++ pkgs/seabird-adventofcode-plugin.nix
@@ -4,13 +4,13 @@
 }:
 buildGoModule rec {
   pname = "seabird-adventofcode-plugin";
-  version = "0.2.3";
+  version = "0.2.4";
 
   src = fetchFromGitHub {
     owner = "seabird-chat";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-o8aPwjiji49ZkWXCP0XMTP2yNP5TU/dQz/qZ1M0Ujqo=";
+    hash = "sha256-BU6AtZXh/EjYtIToCf3KNlDNwb0sqEpGbqByzSqlx2w=";
   };
 
   vendorHash = "sha256-fPWrFzqQizc1206vL3UcQWxIvlUKPzpmq1Kc1V/+cDg=";
