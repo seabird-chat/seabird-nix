53cc690358cb3ad9cc831223dcfa4dc23bb98284
Kaleb Elwert <belak@coded.io>
2026-03-17 23:28:49 -0700

    Bump seabird-webhook-receiver to v0.3.0

--- pkgs/seabird-webhook-receiver.nix
+++ pkgs/seabird-webhook-receiver.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "seabird-webhook-receiver";
-  version = "0.2.1";
+  version = "0.3.0";
 
   src = fetchFromGitHub {
     owner = "seabird-chat";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-93RTNFWiMQ7fKv31HTP6cNPdX1DeN7vPFh1MxM+O9JE=";
+    hash = "sha256-VZRl/ehNnY/uuCw1FAJCL8+6eE9cmOtG73jhT9OHpF8=";
   };
 
   vendorHash = "sha256-ZSX2CD6fLlYsi9IS5i6+A8WlefafXt6zcg6yZQ8oA7Q=";
