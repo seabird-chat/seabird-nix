6a3a09702cf85d7d849c108d9563bb3bc841c757
Jared Ledvina <jared@techsmix.net>
2026-07-24 23:55:38 -0400

    pkgs: upgrade stock to v0.6.0 and datadog to v0.4.0

--- pkgs/seabird-datadog-plugin.nix
+++ pkgs/seabird-datadog-plugin.nix
@@ -5,16 +5,16 @@
 
 seabirdBuildGoModule rec {
   pname = "seabird-datadog-plugin";
-  version = "0.3.0";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "seabird-chat";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-AUkfHDQJtt2SPyHITG/+e2VJzzQ6AXgH/O4YLMGzSZM=";
+    hash = "sha256-3n/FdnjMqeCnXC7jU/bmUIOf2F5BHCquivjuakLyof4=";
   };
 
-  vendorHash = "sha256-8VFxh/htBJzvqklRtK4phGki/tM6sjkPEH0pZmJkmZo=";
+  vendorHash = "sha256-pV7b0wNflcDTZe1Qcnf7Zu1ATDegoytIkdmcu4K5pms=";
 
   subPackages = [
     "cmd/${pname}"
